1cd14e1313a6d49929541019fd8c6ac74a13e075
Max <sonthaya.nt@hotmail.com>
2020-10-30 21:36:13 +0700

    Add progressbar

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,17 @@
+import NextNprogress from 'nextjs-progressbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../css/style.css';
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <NextNprogress
+        color="#29D"
+        startPosition={0.3}
+        stopDelayMs={200}
+        height={3}
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
