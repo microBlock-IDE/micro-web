beee343ec194b6cd7a3b462356219b891d0a9b10
Max <sonthaya.nt@hotmail.com>
2020-11-02 03:57:35 +0700

    Add card link style

--- component/TopicBox.js
+++ component/TopicBox.js
@@ -2,13 +2,18 @@ import { Card } from 'react-bootstrap';
 import Link from 'next/link'
 
 export default function TopicBox(props) {
+
   return (
-    <Card className="mb-3">
-      <Card.Img variant="top" src={props.cover} />
-      <Card.Body>
-        <Card.Title className="card-title" dangerouslySetInnerHTML={{ __html: props.title }}></Card.Title>
-        <Card.Text className="card-text"><span dangerouslySetInnerHTML={{ __html: props.text }}></span> <Link href={`/post/${props.id}/${encodeURI(props.title)}`}>อ่านต่อ</Link></Card.Text>
-      </Card.Body>
-    </Card>
+    <Link href={`/post/${props.id}/${encodeURI(props.title)}`} passHref>
+      <a className="card-link">
+        <Card className="mb-3">
+          <Card.Img variant="top" src={props.cover} />
+          <Card.Body>
+            <Card.Title className="card-title" dangerouslySetInnerHTML={{ __html: props.title }}></Card.Title>
+            <Card.Text className="card-text"><span dangerouslySetInnerHTML={{ __html: props.text }}></span></Card.Text>
+          </Card.Body>
+        </Card>
+      </a>
+    </Link>
   )
 }
